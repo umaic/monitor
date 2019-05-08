1f18def0be54c53b065fff71967716ebfed69cfd
Yezid Carrillo <yecarrillo@gmail.com>
2019-05-08 13:48:34 -0500

    Agregar tracking GA a portal2

--- portal2.php
+++ portal2.php
@@ -40,6 +40,15 @@
         font-size: 11px;
     }
 </style>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139902127-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-139902127-1');
+    </script>
 </head>
 <body>
     <div id="monitor">
